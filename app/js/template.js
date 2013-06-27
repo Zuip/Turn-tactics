63942ad0d2a017a670c12e5659859da0cc096659
Sanep <sanepps@gmail.com>
2013-06-27 22:41:06 +0300

    Edited templating to use html5 history to use same links in pages with and without ajax

--- app/js/template.js
+++ app/js/template.js
@@ -19,37 +19,51 @@ function loadFile(page) {
 }
 
 function updateLinks() {
-	$('a.inner_link').each(function () {
-		var oldText = $(this).attr("href");
-		$(this).attr("href", "#" + oldText.substr(1));
+	$("a.inner_link").unbind('click');
+	$("a.inner_link").on("click", function(event){
+		event.preventDefault();
+		var url = $(this).attr('href');
+		var page = url.substr(1);
+		buildPage(page);
 	});
+
 }
 
 // set default page and transform links
 $(function() {
 
 	updateLinks();
+	// if user moved backwards or forwards
+	window.addEventListener('popstate', function(event) {
+		var page = event.state.page;
+		buildPage(page, false);
+	});
+
 	var search = window.location.search;
-	var hash = window.location.hash.substring(1);
-	if (hash.length != 0) {
-			buildPage(hash);
-	} else if (search.length != 0) {
+	if (search.length != 0) {
 		if (search.substr(1) in pages) {
 			if (typeof pages[search.substr(1)].init == 'function') {
 				pages[search.substr(1)].init();
 			}
 			currentPage = search.substr(1);
 		}
+		history.replaceState({page: currentPage}, "?"+currentPage, "?"+currentPage);
 	} else {
 		currentPage = "index";
 		pages["index"].init();
+		// if user had no page set, leave it that way in browser url bar
+		history.replaceState({page: "index"}, "", "");
 	}
 	firstInit = false;
 	
 });
 
 // load a page
-function buildPage(page) {
+function buildPage(page, saveState = true) {
+	if (saveState == true) {
+		history.pushState({page: page}, "?"+page, "?"+page);
+	}
+	popTwice = true;
 	if (page in pages) {
 		if (firstInit == false) {
 			if (typeof pages[currentPage].clean == 'function') { 
